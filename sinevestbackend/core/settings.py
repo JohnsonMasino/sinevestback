685d1f8dc0174476295d7c021cdbdd0ca1b83ae3
Johnson Masino <johnsonmasino@gmail.com>
2026-08-14 14:31:13 +0100

    Added Database Settings

--- sinevestbackend/core/settings.py
+++ sinevestbackend/core/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,12 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=0,
+        ssl_require=True,
+    )
 }
-
 
 AUTH_USER_MODEL = "authentication.User"
 
